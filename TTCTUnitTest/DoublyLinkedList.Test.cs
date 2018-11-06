0cedbf08b153ebfd43ab45485dc6e8174713c0cd
Tyler Barham <barhamtyler@gmail.com>
2018-11-07 11:04:35 +1300

    Updated the test setup to make use of TestInitialize

--- TTCTUnitTest/DoublyLinkedList.Test.cs
+++ TTCTUnitTest/DoublyLinkedList.Test.cs
@@ -15,15 +15,19 @@ namespace TTCTUnitTest
         }
 
         //Method to call at the start of each test. Ensures that _dll is clean
-        private void setup()
+        [TestInitialize]
+        public void Initialize()
         {
             _dll = new DoublyLinkedList();
         }
 
         //Helper function to store items in the dll
-        private void insertItems(int amount)
+        private void insertItemsToTail(int amount)
         {
-            for(int i = 0; i < amount; i++)
+            //Get the current size of the dll
+            int curr_size = _dll.size();
+
+            for(int i = curr_size; i < amount + curr_size; i++)
             {
                 _dll.pushTail(i.ToString());
             }
@@ -41,15 +45,14 @@ namespace TTCTUnitTest
         [TestMethod]
         public void testLocalHelperFunctions()
         {
-            setup();
             int dllSize = 3;
 
             //Add n items to dll
-            insertItems(dllSize);
+            insertItemsToTail(dllSize);
             Assert.AreEqual(dllSize, _dll.size());
 
             //Add n more items to dll
-            insertItems(dllSize);
+            insertItemsToTail(dllSize);
             Assert.AreEqual((2 * dllSize), _dll.size());
 
             //Remove all and assert size is 0
@@ -58,10 +61,8 @@ namespace TTCTUnitTest
         }
         
         [TestMethod]
-        public void testSizeUpdates()
-        {   
-            setup();
-
+        public void dllSizeUpdates()
+        {
             //Ensure initial size
             Assert.AreEqual(0, _dll.size());
             
@@ -78,10 +79,8 @@ namespace TTCTUnitTest
         }
 
         [TestMethod]
-        public void removeFromEmptyDll()
+        public void dllRemoveFromEmptyList()
         {
-            setup();
-
             //Ensure initial size
             Assert.IsTrue(_dll.isEmpty());
             Assert.AreEqual(0, _dll.size());
@@ -98,8 +97,6 @@ namespace TTCTUnitTest
         [TestMethod]
         public void dllContains()
         {
-            setup();
-
             string term = "A_TEST_TERM";
 
             //Add the term and ensure it is contained in dll
@@ -111,7 +108,158 @@ namespace TTCTUnitTest
             Assert.IsFalse(_dll.contains(term + "1"));
         }
 
-        //_dll.remove(term);
-        //_dll.getAllTerms();
+        [TestMethod]
+        public void dllRemoveNonexistant()
+        {
+            string badTerm = "NOT_IN_DLL";
+
+            //Insert items and ensure the dll doesn't contain the bad term
+            insertItemsToTail(3);
+            Assert.IsFalse(_dll.contains(badTerm));
+
+            //Get dll size
+            int dllSize = _dll.size();
+
+            //Try remove the bad term
+            _dll.remove(badTerm);
+
+            //Assert size hasn't changed
+            Assert.AreEqual(dllSize, _dll.size());
+        }
+
+        [TestMethod]
+        public void dllRemoveFromHead()
+        {
+            string term = "DLL_TERM";
+
+            //Insert items
+            insertItemsToTail(3);
+
+            //Ensure the dll doesn't contain the term, add it, then ensure it is contained
+            Assert.IsFalse(_dll.contains(term));
+            _dll.pushHead(term);
+            Assert.IsTrue(_dll.contains(term));
+
+            //Get dll size
+            int dllSize = _dll.size();
+
+            //Remove the term, make sure it is no longer in the dll
+            _dll.remove(term);
+            Assert.IsFalse(_dll.contains(term));
+
+            //Assert size has been updated
+            Assert.AreEqual(dllSize - 1, _dll.size());
+        }
+
+        [TestMethod]
+        public void dllRemoveFromTail()
+        {
+            string term = "DLL_TERM";
+
+            //Insert items
+            insertItemsToTail(3);
+
+            //Ensure the dll doesn't contain the term, add it, then ensure it is contained
+            Assert.IsFalse(_dll.contains(term));
+            _dll.pushTail(term);
+            Assert.IsTrue(_dll.contains(term));
+
+            //Get dll size
+            int dllSize = _dll.size();
+
+            //Remove the term, make sure it is no longer in the dll
+            _dll.remove(term);
+            Assert.IsFalse(_dll.contains(term));
+
+            //Assert size has been updated
+            Assert.AreEqual(dllSize - 1, _dll.size());
+        }
+
+        [TestMethod]
+        public void dllRemoveFromMiddle()
+        {
+            string term = "DLL_TERM";
+
+            //Insert items
+            insertItemsToTail(3);
+
+            //Ensure the dll doesn't contain the term, add it, then ensure it is contained
+            Assert.IsFalse(_dll.contains(term));
+            _dll.pushTail(term);
+            Assert.IsTrue(_dll.contains(term));
+
+            //Add more items to ensure the term will be removed from the middle
+            insertItemsToTail(3);
+
+            //Get dll size
+            int dllSize = _dll.size();
+
+            //Remove the term, make sure it is no longer in the dll
+            _dll.remove(term);
+            Assert.IsFalse(_dll.contains(term));
+
+            //Assert size has been updated
+            Assert.AreEqual(dllSize - 1, _dll.size());
+        }
+
+        [TestMethod]
+        public void dllgetAllTerms()
+        {
+            string[] terms = { "A", "B", "C", "D" };
+
+            //Ensure the dll is empty
+            Assert.IsTrue(_dll.isEmpty());
+
+            //Add all terms
+            foreach(string term in terms)
+            {
+                _dll.pushHead(term);
+            }
+
+            string allDllTerms = _dll.getAllTerms();
+
+            foreach(string term in terms)
+            {
+                //Assert that the term is contained in the dll
+                Assert.IsTrue(_dll.contains(term));
+                //Assert that the term is contained in the output string of the dll
+                Assert.IsTrue(allDllTerms.Contains(term));
+            }
+        }
+
+        [TestMethod]
+        public void dllgetAllTermsEmpty()
+        {
+            string[] terms = { "A", "B", "C", "D" };
+
+            //Ensure the dll is empty
+            Assert.IsTrue(_dll.isEmpty());
+
+            //Add all terms
+            foreach(string term in terms)
+            {
+                _dll.pushHead(term);
+            }
+
+            int dllSize = _dll.size();
+
+            //Assert that the size of the terms array and the dll match
+            Assert.AreEqual(terms.Length, dllSize);
+
+            //Remove all items and assert that the dll is empty
+            removeAll();
+            Assert.IsTrue(_dll.isEmpty());
+            
+            //Get all the terms
+            string allDllTerms = _dll.getAllTerms();
+
+            foreach(string term in terms)
+            {
+                //Assert that the term is not contained in the dll
+                Assert.IsFalse(_dll.contains(term));
+                //Assert that the term is not contained in the output string of the dll
+                Assert.IsFalse(allDllTerms.Contains(term));
+            }
+        }
     }
 }
